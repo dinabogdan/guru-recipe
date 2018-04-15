d314aea55dc2ecab43077c9a6a57af2dca85350f
dinabogdan <bogdan.dina03@gmail.com>
2018-04-15 13:45:40 +0300

    add command objects

---
+++ src/main/java/com/freesoft/recipe/command/UnitMeasureCommand.java
@@ -0,0 +1,13 @@
+package com.freesoft.recipe.command;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UnitMeasureCommand {
+    private Long id;
+    private String description;
+}
