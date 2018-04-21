8d72c130c06d26204ee3ac9a3ee1fbd26ab763d7
dinabogdan <bogdan.dina03@gmail.com>
2018-04-21 19:57:31 +0300

    some dev

--- src/main/java/com/freesoft/recipe/control/RecipeController.java
+++ src/main/java/com/freesoft/recipe/control/RecipeController.java
@@ -5,8 +5,13 @@ import com.freesoft.recipe.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @Slf4j
@@ -39,7 +44,14 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    public String saveOrUpdate(@Valid @ModelAttribute RecipeCommand command,
+                               BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(e -> {
+                log.error(e.toString());
+            });
+            return "recipe-form";
+        }
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
@@ -49,5 +61,4 @@ public class RecipeController {
         recipeService.deleteById(new Long(id));
         return "redirect:/";
     }
-
 }
